1fd272b5d8b8ff4aa9bf5fe80a3b85a4b0d6370e
Albert Besuhoff <albert.besuhoff@gmail.com>
2025-12-26 16:38:59 +0200

    Draw a pointer to player's dropped bonuses

--- src/types/screen-objects/IBonus.ts
+++ src/types/screen-objects/IBonus.ts
@@ -6,6 +6,7 @@ import { Bonus as BonusMessage } from "../../types/socketEvents";
 export type BonusType = "aid_kit" | "goggles" | "chest";
 export interface IBonus extends IScreenObject, IDrawable {
   type: BonusType;
+  belongsToPlayer: boolean;
 }
 
 export interface IBonusFactory {
--- src/utils/geometry/Point2D.ts
+++ src/utils/geometry/Point2D.ts
@@ -1,96 +1,108 @@
 import { IPoint } from "../../types/geometry/IPoint";
 
 export class Point2D implements IPoint {
-    constructor(private _x: number, private _y: number) {
-    }
+  constructor(
+    private _x: number,
+    private _y: number
+  ) {}
 
-    public get x(): number {
-        return this._x;
-    }
+  public get x(): number {
+    return this._x;
+  }
 
-    public get y(): number {
-        return this._y;
-    }
+  public get y(): number {
+    return this._y;
+  }
 
-    toString(): string {
-        return `(${this._x.toFixed(2)}, ${this._y.toFixed(2)})`;
-    }
+  toString(): string {
+    return `(${this._x.toFixed(2)}, ${this._y.toFixed(2)})`;
+  }
 
-    equals(point: IPoint): boolean {
-        return this._x === point.x && this._y === point.y;
-    }
+  equals(point: IPoint): boolean {
+    return this._x === point.x && this._y === point.y;
+  }
 
-    clone(): Point2D {
-        return new Point2D(this._x, this._y);
-    }
+  subtract(point: IPoint): Point2D {
+    this._x -= point.x;
+    this._y -= point.y;
+    return this;
+  }
 
-    setTo(x: number, y: number): Point2D {
-        this._x = x;
-        this._y = y;
-        return this;
-    }
+  subtracted(point: IPoint): Point2D {
+    return this.clone().subtract(point);
+  }
 
-    setToPointCoordinates(point: Point2D): Point2D {
-        return this.setTo(point.x, point.y);
-    }
+  clone(): Point2D {
+    return new Point2D(this._x, this._y);
+  }
 
-    moveBy(dx: number, dy: number): Point2D {
-        this._x += dx;
-        this._y += dy;
-        return this;
-    }
+  setTo(x: number, y: number): Point2D {
+    this._x = x;
+    this._y = y;
+    return this;
+  }
 
-    movedBy(dx: number, dy: number): Point2D {
-        return this.clone().moveBy(dx, dy);
-    }
+  setToPointCoordinates(point: Point2D): Point2D {
+    return this.setTo(point.x, point.y);
+  }
 
-    moveByPointCoordinates(point: Point2D): Point2D {
-        return this.moveBy(point.x, point.y);
-    }
+  moveBy(dx: number, dy: number): Point2D {
+    this._x += dx;
+    this._y += dy;
+    return this;
+  }
 
-    movedByPointCoordinates(point: Point2D): Point2D {
-        return this.clone().moveByPointCoordinates(point);
-    }
+  movedBy(dx: number, dy: number): Point2D {
+    return this.clone().moveBy(dx, dy);
+  }
 
-    invertAgainstPointCoordinates(center: Point2D): Point2D {
-        return this.moveBy(2 * (center.x - this._x), 2 * (center.y - this._y));
-    }
+  moveByPointCoordinates(point: Point2D): Point2D {
+    return this.moveBy(point.x, point.y);
+  }
 
-    invertedAgainstPointCoordinates(center: Point2D): Point2D {
-        return this.clone().invertAgainstPointCoordinates(center);
-    }
+  movedByPointCoordinates(point: Point2D): Point2D {
+    return this.clone().moveByPointCoordinates(point);
+  }
 
-    invert(): Point2D {
-        return this.invertAgainstPointCoordinates(new Point2D(0, 0));
-    }
+  invertAgainstPointCoordinates(center: Point2D): Point2D {
+    return this.moveBy(2 * (center.x - this._x), 2 * (center.y - this._y));
+  }
 
-    inverted(): Point2D {
-        return this.clone().invert();
-    }
+  invertedAgainstPointCoordinates(center: Point2D): Point2D {
+    return this.clone().invertAgainstPointCoordinates(center);
+  }
 
-    rotateAroundPointCoordinates(center: Point2D, angle: number): Point2D {
-        const cos = Math.cos(angle * Math.PI / 180);
-        const sin = Math.sin(angle * Math.PI / 180);
-        const dx = this._x - center._x;
-        const dy = this._y - center.y;
-        this._x = dx * cos - dy * sin + center._x;
-        this._y = dx * sin + dy * cos + center.y;
-        return this;
-    }
+  invert(): Point2D {
+    return this.invertAgainstPointCoordinates(new Point2D(0, 0));
+  }
 
-    rotatedAroundPointCoordinates(center: Point2D, angle: number): Point2D {
-        return this.clone().rotateAroundPointCoordinates(center, angle);
-    }
+  inverted(): Point2D {
+    return this.clone().invert();
+  }
 
-    rotate(angle: number): Point2D {
-        return this.rotateAroundPointCoordinates(new Point2D(0, 0), angle);
-    }
+  rotateAroundPointCoordinates(center: Point2D, angle: number): Point2D {
+    const cos = Math.cos((angle * Math.PI) / 180);
+    const sin = Math.sin((angle * Math.PI) / 180);
+    const dx = this._x - center._x;
+    const dy = this._y - center.y;
+    this._x = dx * cos - dy * sin + center._x;
+    this._y = dx * sin + dy * cos + center.y;
+    return this;
+  }
 
-    rotated(angle: number): Point2D {
-        return this.clone().rotate(angle);
-    }
+  rotatedAroundPointCoordinates(center: Point2D, angle: number): Point2D {
+    return this.clone().rotateAroundPointCoordinates(center, angle);
+  }
 
-    distanceTo(point: IPoint): number {
-        return Math.sqrt((this._x - point.x) ** 2 + (this.y - point.y) ** 2);
-    }
+  rotate(angle: number): Point2D {
+    return this.rotateAroundPointCoordinates(new Point2D(0, 0), angle);
+  }
+
+  rotated(angle: number): Point2D {
+    return this.clone().rotate(angle);
+  }
+
+  distanceTo(point: IPoint): number {
+    return Math.sqrt((this._x - point.x) ** 2 + (this.y - point.y) ** 2);
+  }
 }
